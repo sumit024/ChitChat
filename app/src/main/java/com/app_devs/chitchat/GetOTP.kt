f14959608470aa7e65ce51d791d98cf758590746
Sumit Kushwaha <kushwahasumit024@gmail.com>
2021-10-04 00:02:31 +0530

    firebase added

--- app/src/main/java/com/app_devs/chitchat/GetOTP.kt
+++ app/src/main/java/com/app_devs/chitchat/GetOTP.kt
@@ -8,7 +8,7 @@ import android.view.ViewGroup
 import com.app_devs.chitchat.R
 
 class GetOTP : Fragment() {
-    //kuch vi
+    //kuch vijvhhqkdbf
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
