50acc025bfa28dcb0d2fa21636ca3abbbc4c2cba
Sumit Kushwaha <kushwahasumit024@gmail.com>
2021-10-03 17:29:21 +0530

    Splash Screen implemented

--- app/src/main/java/com/app_devs/chitchat/MainActivity.kt
+++ app/src/main/java/com/app_devs/chitchat/MainActivity.kt
@@ -2,10 +2,13 @@ package com.app_devs.chitchat
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
     }
 }
