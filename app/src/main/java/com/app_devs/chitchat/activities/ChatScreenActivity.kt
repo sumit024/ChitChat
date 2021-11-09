cffafa4416fe7ac31daa2fc270aee51a327a3147
Sumit Kushwaha <kushwahasumit024@gmail.com>
2021-11-09 17:59:18 +0530

    Bottom navigation fragment done!

--- app/src/main/java/com/app_devs/chitchat/activities/ChatScreenActivity.kt
+++ app/src/main/java/com/app_devs/chitchat/activities/ChatScreenActivity.kt
@@ -2,11 +2,23 @@ package com.app_devs.chitchat.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
 import com.app_devs.chitchat.R
+import com.app_devs.chitchat.databinding.ActivityChatScreenBinding
 
 class ChatScreenActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityChatScreenBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_chat_screen)
+        binding= ActivityChatScreenBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        val navController=findNavController(R.id.hostFragment)
+        binding.bottomNavigation.setupWithNavController(navController)
+        val appBarConfiguration= AppBarConfiguration(setOf(R.id.chatFragment,R.id.profileFragment))
+        setupActionBarWithNavController(navController,appBarConfiguration)
+
     }
 }
